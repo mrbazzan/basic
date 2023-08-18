5dd008b9d05c66c28189ad6d5951239e29deb70e
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-08-18 15:00:35 +0100

    flipper.py: change from an OOP approach to a functional one

--- remo/flipper.py
+++ remo/flipper.py
@@ -1,40 +1,29 @@
 import random
 import tkinter as tk
 
+def create_gui():
+    root = tk.Tk()
+    root.title("Color Flipper")
+    root.geometry("800x600")
 
-class FlipperApp:
+    def flip_colors():
+        colors = ["red", "green", "blue", "yellow", "orange"]
+        label.configure(background=random.choice(colors))
 
-    def __init__(self, master):
-        self.root = master
-        self.root.title("Color Flipper")
-        self.root.geometry("800x600")
+    label = tk.Label(
+        root,
+        text="Click to flip colors",
+        height=10, width=60,
+        bd=2, relief=tk.RAISED
+    )
+    label.grid(row=0, column=1, padx=10, pady=10)
 
+    button = tk.Button(
+        root,
+        text="Flip Colors", command=flip_colors
+    )
+    button.grid(row=1, column=1, padx=10, pady=10)
 
-    def run(self):
+    root.mainloop()
 
-        def flip_colors():
-            colors = ["red", "green", "blue", "yellow", "orange"]
-            label.configure(background=random.choice(colors))
-
-
-        label = tk.Label(
-            self.root, 
-            text="Click to flip colors", 
-            height=10, width=60,
-            bd=2, relief=tk.RAISED
-        )
-        label.grid(row=0, column=1, padx=10, pady=10)
-
-        button = tk.Button(
-            self.root,
-            text="Flip Colors", command=flip_colors
-        )
-        button.grid(row=1, column=1, padx=10, pady=10)
-
-        self.root.mainloop()
-
-
-root = tk.Tk()
-app = FlipperApp(root)
-app.run()
-
+create_gui()
