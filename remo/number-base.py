07b946d07fd2bc23b9b2d74d7ae8a170dae2e2bb
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-09-01 00:21:57 +0100

    more number systems

--- remo/number-base.py
+++ remo/number-base.py
@@ -27,3 +27,32 @@ class NumberBaseConverter:
 
         return cls.return_result(result)
 
+    @classmethod
+    def base_seven(cls, number):
+        result = []
+        while number > 0:
+            remainder = number % 7
+            result.insert(0, str(remainder))
+            number = number // 7
+        return cls.return_result(result)
+
+    @classmethod
+    def base_four(cls, number):
+        result = []
+        while number > 0:
+            remainder = number % 4
+            result.insert(0, str(remainder))
+            number = number // 4
+        return cls.return_result(result)
+
+    @classmethod
+    def base_six(cls, number):
+        result = []
+        while number > 0:
+            remainder = number % 6
+            result.insert(0, str(remainder))
+            number = number // 6
+        return cls.return_result(result)
+
+
+
