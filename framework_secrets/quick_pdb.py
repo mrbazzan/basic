d0efe9040c971aad214cbf831e032b97cb5b43d3
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-10-02 09:54:55 +0100

    Quick introduction to pdb module
    Courtesy of
       "Secrets of the framework creator"

---
+++ framework_secrets/quick_pdb.py
@@ -0,0 +1,24 @@
+
+import pdb
+
+print("Hello world")
+
+pdb.set_trace()
+
+t = 0
+for i in range(5, 11):
+    print(i)
+    t += i
+
+print(t)
+
+######################################
+# COMMANDS TO NOTE                  ##
+#                                   ##
+# h - help                          ##
+# l - list                          ##
+# c - continue to end/breakpoint    ##
+# n - next line                     ##
+# q - quit                          ##
+#                                   ##
+######################################
