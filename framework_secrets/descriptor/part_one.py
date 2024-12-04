2a49902edb4a733f65c78bf16c9fde381f09ec01
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-12-04 10:23:23 +0100

    more on descriptor protocol

--- framework_secrets/descriptor/part_one.py
+++ framework_secrets/descriptor/part_one.py
@@ -6,19 +6,30 @@ class Die:
         self.sides = sides
 
     def __get__(self, instance, owner):
-        print(instance, owner)
+        print(f"INSTANCE: {instance}, OWNER: {owner}")
         return int(random.random() * self.sides)
 
 
 class Game:
+    def __init__(self):
+        self.x = 8
+        # self.d6 = 8  # print(g.d6)  # g.__dict__['d6']
+
     d6 = Die()
     d10 = Die(10)
 
 
 if __name__ == "__main__":
-    print(Game.d6)
+    print(Game.d6)  # --> Game.__dict__['d6'].__get__(None, Game)
     print(Game.d10)
 
-    game = Game()
-    print(game.d6)
+    g = Game()
+    print(g.d6)  # {g.__dict__['d6'] | type(g).__dict__['d6']}.__get__(g, type(g))
+
+    print(g.__dict__)
+    g.d6 = 5  # --> non-data descriptor overriden by instance dictionary
+    print(g.d6)
+
+    # entries in instance dictionary
+       # data descriptor > instance dictionary > non-data descriptor
 
