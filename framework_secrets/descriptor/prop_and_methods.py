4106532a50dd0c30da7dbfe518ede93cad6bfbad
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-12-05 11:41:06 +0100

    descriptor protocol: property, staticmethod, classmethod
    It is used in bounded and unbounded functions too
    
    class Function:
       ...
       def __get__(self, obj, typeobj):
           ...

---
+++ framework_secrets/descriptor/prop_and_methods.py
@@ -0,0 +1,67 @@
+
+#
+# PROPERTY
+#
+
+class Property:
+    def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
+        self.__doc__ = doc
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self.fget is None:
+            raise AttributeError("unreadable attribute")
+        return self.fget(obj)
+
+    def __set__(self, obj, value):
+        if self.fset is None:
+            raise AttributeError("can't set attribute")
+        self.fset(obj, value)
+
+    def __delete__(self, obj):
+        if self.fdel is None:
+            raise AttributeError("can't delete attribute")
+        self.fdel(obj)
+
+
+class Game:
+    def getx(self): return self._x
+    def setx(self, val): self._x = val,
+    def delx(self): del self._x
+    d6 = Property(getx, setx, delx)
+
+
+if __name__ == "__main__":
+    print(Game.d6)
+
+    g = Game()
+    print(g.d6)
+
+
+#
+# STATIC METHODS AND CLASS METHODS
+#
+# STATIC METHODS
+#
+
+class StaticMethod:
+    def __init__(self, func):
+        self.func = func
+    def __get__(self, obj, objtype=None):
+        return self.func
+
+#
+# CLASS METHODS
+#
+
+class ClassMethod:
+    def __init__(self, func):
+        self.func = func
+    def __get__(self, obj, objtype=None):
+        if objtype is None: objtype = type(obj)
+        return lambda *args: self.func(objtype, *args)
+
