f0458bc58c9ad6012748b506234cf312c5d25135
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-12-31 10:30:38 +0100

    object model: Test read and write on class

--- framework_secrets/obj_model/test_klass.py
+++ framework_secrets/obj_model/test_klass.py
@@ -34,3 +34,17 @@ class TestObjectModel(unittest.TestCase):
         self.assertEqual(b_obj.read_attr('a'), 2)
         self.assertEqual(b_obj.read_attr('b'), 5)
 
+    def test_read_and_write_field_class(self):
+        # Normal PYTHON IMPL.
+        class A:
+            pass
+        A.a = 1
+        self.assertEqual(A.a, 1)
+        A.a = 6
+        self.assertEqual(A.a, 6)
+
+        # Custom IMPL
+        B = Class(name="B", base_class=OBJECT, fields={'a':1}, metaclass=TYPE)
+        self.assertEqual(B.read_attr('a'), 1)
+        B.write_attr('a', 6)
+        self.assertEqual(B.read_attr('a'), 6)
