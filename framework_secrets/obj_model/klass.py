ea2c6ea044b320602f5e8c3d8903b2c77baa1a01
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2025-01-02 10:17:44 +0100

    object model: Implement and test calling methods

--- framework_secrets/obj_model/klass.py
+++ framework_secrets/obj_model/klass.py
@@ -8,6 +8,10 @@ class Base:
     def isinstance(self, cls):
         return self.cls.issubclass(cls)
 
+    def call_method(self, method_name, *args):
+        method = self.cls._read_from_class(method_name)
+        return method(self, *args)
+
     def _read_dict(self, field_name):
         return self._fields.get(field_name, MISSING)
 
@@ -33,6 +37,12 @@ class Class(Base):
         Base.__init__(self, metaclass, fields)
         self.name = name
         self.base_class = base_class
+
+    def _read_from_class(self, method_name):
+        for cls in self.mro():
+            if method_name in cls._fields:
+                return cls._fields[method_name]
+        return MISSING
 
     def mro(self):
         if self.base_class is None:
--- framework_secrets/obj_model/test_klass.py
+++ framework_secrets/obj_model/test_klass.py
@@ -72,3 +72,32 @@ class TestObjectModel(unittest.TestCase):
         self.assertTrue(_b.isinstance(_A))
         self.assertTrue(_b.isinstance(OBJECT))
         self.assertFalse(_b.isinstance(TYPE))
+
+    def test_call_method(self):
+        # Normal PYTHON IMPL
+        class A:
+            def f(self):
+                return self.x + 1
+        a = A()
+        a.x = 1
+        self.assertEqual(a.f(), 2)
+
+        class B(A):
+            pass
+
+        b = B()
+        b.x = 1
+        self.assertEqual(b.f(), 2)
+
+        # Custom IMPL.
+        def f(self):
+            return self.read_attr('x') + 1
+        _A = Class(name="_A", base_class=OBJECT, fields={'f': f}, metaclass=TYPE)
+        _a = Instance(_A)
+        _a.write_attr('x', 1)
+        self.assertEqual(_a.call_method('f'), 2)
+
+        _B = Class(name="_B", base_class=_A, fields={}, metaclass=TYPE)
+        _b = Instance(_B)
+        _b.write_attr('x', 1)
+        self.assertEqual(_b.call_method('f'), 2)
