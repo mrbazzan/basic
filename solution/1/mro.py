b95bcf7238f720937622cee114c5b8125984b13b
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2025-07-24 01:28:07 +0100

    c3 algorithm: part II

--- solution/1/mro.py
+++ solution/1/mro.py
@@ -2,7 +2,25 @@
 # C3 ALGORITHM
 
 def merge(seqs):
-    pass
+
+    res = [];
+    while True:
+      nonemptyseqs=[seq for seq in seqs if seq]
+      if not nonemptyseqs: return res
+
+      # find merge candidates among seq heads
+      for seq in nonemptyseqs:
+          cand = seq[0]
+          nothead=[s for s in nonemptyseqs if cand in s[1:]]
+
+          if nothead: cand=None #reject candidate
+          else: break
+
+      if not cand: raise "Inconsistent hierarchy"
+      res.append(cand)
+      for seq in nonemptyseqs: # remove cand
+          if seq[0] == cand: del seq[0]
+
 
 def mro(k):
     def inner(klass):
@@ -11,12 +29,13 @@ def mro(k):
             b += [[k] + inner(k.__bases__)]
         return b
 
-    return merge([[k]] + inner(k.__bases__) + [list(k.__bases__)])
+    return merge([[k]] + [inner(k.__bases__)] + [list(k.__bases__)])
 
 class Example:
     class D: pass
     class E: pass
     class F: pass
+    class X(F): pass
     class B(D,E): pass
     class C(D,F): pass
     class A(B,C): pass
